a91ec7ce7b8690ae101e1826c59097cc732064a9
K <kainyame@gmail.com>
2017-05-14 10:08:43 -0700

    Had Unity Lesson #1
    New Project file in Unity folder

---
+++ Unity/NewUnityProject/Assets/Scripts/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+	int updates = 0;
+	Rigidbody myRigidBody;
+
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log("in start");
+		myRigidBody = GetComponent<Rigidbody>();
+		Invoke("DeleteMe", 10);
+	}
+	
+	// extra functions 
+	public void AddForceToMe(Vector3 force){
+	myRigidBody.AddForce(force);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("update: " + updates);
+		updates++;
+	}
+
+	voide DeleteMe(){
+		Destroy(gameObject);
+	}
+}
---
+++ Unity/NewUnityProject/Assets/Scripts/SpawnBoxes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBoxes : MonoBehaviour {
+	[SerializeField]
+	GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnBox());
+	}
+
+	//random spawning
+	IEnumerator SpawnBox(){
+		GameObject spawnedBox = Instantiate(box, transform.position, transform.rotation);
+		int forceX = Random.Range(-1000, 1001);
+		int forceY = Random.Range(-1000, 1001);
+		int forceZ = Random.Range(-1000, 1001);
+		Vector3 forceToAdd = new Vector3(forceX, forceY, forceZ);
+		//this is pretty important
+		spawnedBox.GetComponent<Box>().AddForceToMe(forceToAdd);
+		yield return new WaitForSeconds(1);
+		StartCoroutine(SpawnBox());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
